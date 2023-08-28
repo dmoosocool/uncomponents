6350ca57d32da3a33acb52a2396c8beeeddb644e
DM <dmoosocool@gmail.com>
2023-08-28 20:39:41 +0800

    chore: upgrade examples

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,6 +3,7 @@ import { WalletAvatar, WalletButton } from "ui";
 export default function Page(): JSX.Element {
   return (
     <>
+      <h1>UnComponents Docs</h1>
       <WalletAvatar />
       <WalletButton />
     </>
